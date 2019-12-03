66349fc38e4711e6f59ccceb3badca19db8eeecb
Erik Reinert <e@drn.io>
2019-12-03 10:43:47 -0800

    #1: Makes dotenv files load dynamically from environment variable

--- main.go
+++ main.go
@@ -3,17 +3,29 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"github.com/joho/godotenv"
 )
 
-func main() {
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
+// LoadDotEnv : See docs https://github.com/joho/godotenv#precendence--conventions
+func LoadDotEnv() {
+	env := os.Getenv("HEALTHCHECK_ENV")
+	if "" == env {
+		env = "development"
 	}
+	godotenv.Load(".env." + env + ".local")
+	if "test" != env {
+		godotenv.Load(".env.local")
+	}
+	godotenv.Load(".env." + env)
+	godotenv.Load()
+}
+
+func main() {
+	LoadDotEnv()
 
 	cli, err := client.NewEnvClient()
 	if err != nil {
